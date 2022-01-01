af47bf4c84cf215a13e405878881d57ce92c505f
flame1833 <victor_lai1833@hotmail.com>
2022-01-01 11:53:43 +0800

    Create update_config.sh

---
+++ update_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd  /usr/local/go/src/github.com/pandotoken/pando/integration/pandonet/node
+rm -rf config.yaml
+wget https://raw.githubusercontent.com/flame1833/MetaInstallation/master/config.yaml
+chmod +x config.yaml
+
+cd  /usr/local/go/src/github.com/pandotoken/pandonet/node
+rm -rf config.yaml
+wget https://raw.githubusercontent.com/flame1833/MetaInstallation/master/config.yaml
+chmod +x config.yaml
+
+cd $PANDO_HOME
+rm -rf /usr/local/go/src/github.com/pandotoken/pandonet/node/db/main/*
+rm -rf /usr/local/go/src/github.com/pandotoken/pandonet/node/db/ref/*
+rm -rf /usr/local/go/src/github.com/pandotoken/pandonet/node/db/peer_table/*
+make install
+
+/usr/local/go/bin/pando start --config=../pandonet/node
